56077151c8b43a1e374859b721ab93843f468288
Hafeez <hafeezahamed5074@gmail.com>
2025-06-10 21:34:26 +0530

    index page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PageController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PageController::class, 'main']);
